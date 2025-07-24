4398210d01e0554f8d1b22c8d2cafdacaf951c4b
Edwin Hu <eddyhu@gmail.com>
2025-07-24 19:39:28 -0400

    Add wezterm CLI wrapper and update flake.lock

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -1,11 +1,17 @@
 { pkgs }:
 
 with pkgs;
-let shared-packages = import ../shared/packages.nix { inherit pkgs; }; in
+let 
+  shared-packages = import ../shared/packages.nix { inherit pkgs; };
+  wezterm-cli = pkgs.writeShellScriptBin "wezterm" ''
+    exec /Applications/WezTerm.app/Contents/MacOS/wezterm "$@"
+  '';
+in
 shared-packages ++ [
   aerospace
   dockutil
   ((emacsPackagesFor emacs-macport).emacsWithPackages (epkgs: [ epkgs.vterm ])) 
   jankyborders
-  sketchybar  
+  sketchybar
+  wezterm-cli
 ]
